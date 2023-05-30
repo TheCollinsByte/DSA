880ba478c780f07ae1da8b1f39b1244c98eb6436
colspan <collin@collindigm.com>
2023-05-30 07:18:40 +0300

    SPFAlgorithm: implmented the calculateRoutes method to return a List of Routes

--- src/main/java/dev/collin/spf/SPFAlgorithm.java
+++ src/main/java/dev/collin/spf/SPFAlgorithm.java
@@ -1,7 +1,18 @@
 package dev.collin.spf;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SPFAlgorithm {
     public static void main(String[] args) {
         System.out.println("Shortest Path First");
+    }
+
+    public List<Route> calculateRoutes(Router router) {
+        List<Route> routes = new ArrayList<Route>();
+        for (Link link : router.getLinks()) {
+            routes.add(new Route(link.getDestination(), link.getCost()));
+        }
+        return routes;
     }
 }
