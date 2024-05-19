696e4d5e22e538b25e8f0616b0de07451e3d3aa7
colspan <collin@collindigm.com>
2024-05-19 11:48:53 +0300

    Stack implementation using an Array

--- src/main/java/com/collindigm/dsa/stack/array/Stack.java
+++ src/main/java/com/collindigm/dsa/stack/array/Stack.java
@@ -1,5 +1,53 @@
 package com.collindigm.dsa.stack.array;
 
-public class Stack {
+import java.util.EmptyStackException;
 
+public class Stack {
+    private int top;
+    private int[] arr;
+
+    public Stack(int capacity)  {
+        top = -1;
+        arr = new int[capacity];
+    }
+
+    public Stack() {
+        this(10);
+    }
+
+    public boolean isFull() {
+        return arr.length == size();
+    }
+
+    public int size() {
+        return top + 1;
+    }
+
+    public boolean isEmpty() {
+        return top < 0;
+    }
+
+    public void push(int data) {
+        if(isFull()) {
+            throw new RuntimeException("Stack is full.");
+        }
+        top++;
+        arr[top] = data;
+    }
+
+    public int pop() {
+        if (isEmpty()) {
+            throw new RuntimeException("Stack is empty!!!");
+        }
+        int result = arr[top];
+        top--;
+        return result;
+    }
+
+    public int peek() {
+        if (isEmpty()) {
+            throw new RuntimeException("Stack is empty!!!");
+        }
+        return arr[top];
+    }
 }
