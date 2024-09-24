175cdccab4c313bcd89cb1b7409f096f3fbbc98e
colspan <collin@collindigm.com>
2024-09-24 07:58:49 +0300

    Arrays: src/main and test cases initialized

---
+++ ds-arrays/src/main/java/com/thecollinsbyte/dsa/Arrays.java
@@ -0,0 +1,46 @@
+package com.thecollinsbyte.dsa;
+
+public class Arrays {
+
+    public int[] declaration() {
+
+        int intArrary[];
+        char characterArray[];
+        float floatArray[];
+
+        return new int[]{1, 2, 3, 4};
+    }
+
+    public int[] initialization() {
+
+        int intArrary[] = {1, 2, 3, 4, 5, 6, 7 , 8, 9, 10};
+        char characterArray[] = {'A', 'B', 'C'};
+        float floatArray[] = {3.4f, 1.2f, 3.8f};
+
+        return new int[]{23, 32, 32};
+    }
+
+    public int[] traversal() {
+
+
+        return new int[]{23, 19, 38};
+    }
+
+    public int[] insertion() {
+
+
+        return new int[]{23, 19, 38};
+    }
+
+    public int[] deletion() {
+
+
+        return new int[]{23, 19, 38};
+    }
+
+    public int[] searching() {
+
+
+        return new int[]{23, 19, 38};
+    }
+}
