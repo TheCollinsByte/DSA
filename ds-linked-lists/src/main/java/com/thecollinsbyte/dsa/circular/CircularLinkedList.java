32f5beeae4111296fc873c5f8c7e2adf1336d72a
colspan <collin@collindigm.com>
2024-09-29 15:11:29 +0300

    Circular Linked List: size, Insertion At the Beginning implementation with test suites

--- ds-linked-lists/src/main/java/com/thecollinsbyte/dsa/circular/CircularLinkedList.java
+++ ds-linked-lists/src/main/java/com/thecollinsbyte/dsa/circular/CircularLinkedList.java
@@ -1,5 +1,8 @@
 package com.thecollinsbyte.dsa.circular;
 
+import java.util.List;
+import java.util.ArrayList;
+
 class Node<T> {
     T data;
     Node next;
@@ -25,7 +28,30 @@ public class CircularLinkedList<T> {
         return size;
     }
 
+    public List<T> traverse() {
+
+        List<T> result = new ArrayList<>();
+
+        if(size == 0) return result;
+        Node<T> current = tail.next;
+        do {
+            result.add(current.data);
+            current = current.next;
+        } while(current != tail.next);
+
+        return result;
+    }
+
     public void insertAtBeginning(T data) {
-        Node<T> newNode = new Node<>();
+        Node<T> newNode = new Node<>(data);
+        if(size == 0) {
+            tail = newNode;
+            tail.next = newNode;
+            size++;
+            return;
+        }
+        newNode.next = tail.next;
+        tail.next = newNode;
+        size++;
     }
 }
--- ds-linked-lists/src/test/java/com/thecollinsbyte/dsa/circular/CircularLinkedListTest.java
+++ ds-linked-lists/src/test/java/com/thecollinsbyte/dsa/circular/CircularLinkedListTest.java
@@ -1,5 +1,39 @@
 package com.thecollinsbyte.dsa.circular;
 
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import com.thecollinsbyte.dsa.circular.CircularLinkedList;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class CircularLinkedListTest {
 
+    CircularLinkedList<Integer> integerList;
+    CircularLinkedList<String> stringList;
+
+    @BeforeEach
+    public void setUp() {
+        integerList = new CircularLinkedList<>();
+        stringList = new CircularLinkedList<>();
+    }
+
+    @Test
+    public void testSize() {
+        integerList.insertAtBeginning(10);
+        integerList.insertAtBeginning(20);
+        integerList.insertAtBeginning(30);
+
+        assertEquals(3, integerList.size());
+    }
+
+    @Test
+    public void testInsertionAtBeginning() {
+        integerList.insertAtBeginning(10);
+        integerList.insertAtBeginning(20);
+        integerList.insertAtBeginning(30);
+
+        List<Integer> result = integerList.traverse();
+
+        assertEquals(List.of(30, 20, 10), result);
+    }
 }
