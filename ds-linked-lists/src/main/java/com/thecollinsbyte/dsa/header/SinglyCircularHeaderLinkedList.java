0f2e307ccba64ca019c1fe2e781389113057566f
colspan <collin@collindigm.com>
2024-10-03 16:53:17 +0300

    Singly Circular Header Linked List: Insertion At Beginning with implementation and test suite

--- ds-linked-lists/src/main/java/com/thecollinsbyte/dsa/header/SinglyCircularHeaderLinkedList.java
+++ ds-linked-lists/src/main/java/com/thecollinsbyte/dsa/header/SinglyCircularHeaderLinkedList.java
@@ -1,4 +1,41 @@
 package com.thecollinsbyte.dsa.header;
 
-public class SinglyCircularHeaderLinkedList {
+
+public class SinglyCircularHeaderLinkedList<T> {
+
+    private static class Node<T> {
+        T data;
+        Node<T> next;
+
+        public Node(T data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    private Node<T> header;
+    private int size;
+
+    public SinglyCircularHeaderLinkedList() {
+        header = new Node<>(null);
+        header.next = header;
+        size = 0;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public void insertAtBeginning(T data) {
+        Node<T> newNode = new Node<>(data);
+        if(header.next == header) {
+            header.next = newNode;
+            newNode.next = header;
+            size++;
+            return;
+        }
+        newNode.next = header.next;
+        header.next = newNode;
+        size++;
+    }
 }
--- ds-linked-lists/src/test/java/com/thecollinsbyte/dsa/header/SinglyCircularHeaderLinkedListTest.java
+++ ds-linked-lists/src/test/java/com/thecollinsbyte/dsa/header/SinglyCircularHeaderLinkedListTest.java
@@ -1,5 +1,27 @@
 package com.thecollinsbyte.dsa.header;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+
 public class SinglyCircularHeaderLinkedListTest {
 
+    private SinglyCircularHeaderLinkedList<Integer> integerList;
+
+    @BeforeEach
+    public void setUp() {
+        integerList = new SinglyCircularHeaderLinkedList<>();
+    }
+
+    @Test
+    public void testInsertAtBeginning() {
+        integerList.insertAtBeginning(10);
+        integerList.insertAtBeginning(20);
+        integerList.insertAtBeginning(30);
+        integerList.insertAtBeginning(40);
+        integerList.insertAtBeginning(50);
+
+        assertEquals(5, integerList.size());
+    }
 }
