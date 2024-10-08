bdfa3eb2243806e8f33a5778249552380ae1906e
colspan <collin@collindigm.com>
2024-10-08 13:12:35 +0300

    Matrix: Insertion At Beginning implemented and fully tested

--- ds-matrix/src/main/java/com/thecollinsbyte/dsa/Matrix.java
+++ ds-matrix/src/main/java/com/thecollinsbyte/dsa/Matrix.java
@@ -1,4 +1,53 @@
 package com.thecollinsbyte.dsa;
 
-public class Matrix<T> {
+public class Matrix<T extends Number> {
+    private int rows;
+    private int cols;
+    private T[][] data;
+
+
+    public Matrix(int rows, int cols, T[][] data) {
+        this.rows = rows;
+        this.cols = cols;
+        this.data = data;
+    }
+
+    public void insertAtBeginning(T element) {
+        if (rows == 0 && cols == 0) {
+            resizeMatrix(1, 1);
+            data[0][0] = element;
+        } else {
+            resizeMatrix(rows + 1, cols);
+            shiftDown(0);
+            for (int i = 0; i < cols; i++) {
+                data[0][i] = element;
+            }
+        }
+    }
+
+    public T[][] getData() {
+        return data;
+    }
+
+    private void resizeMatrix(int newRows, int newCols) {
+        T[][] newData = createEmptyMatrix(newRows, newCols);
+
+        for (int i = 0; i < Math.min(rows, newRows); i++) {
+            System.arraycopy(data[i], 0, newData[i], 0, Math.min(cols, newCols));
+        }
+        this.data = newData;
+        this.rows = newRows;
+        this.cols = newCols;
+    }
+
+    private void shiftDown(int fromRow) {
+        for (int i = rows - 1; i > fromRow; i--) {
+            data[i] = data[i - 1];
+        }
+        data[fromRow] = createEmptyMatrix(1, cols)[0];
+    }
+
+    private T[][] createEmptyMatrix(int rows, int cols) {
+         return (T[][]) new Number[rows][cols];
+    }
 }
--- ds-matrix/src/test/java/com/thecollinsbyte/dsa/MatrixTest.java
+++ ds-matrix/src/test/java/com/thecollinsbyte/dsa/MatrixTest.java
@@ -1,4 +1,27 @@
 package com.thecollinsbyte.dsa;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+import org.junit.jupiter.api.Test;
+
 public class MatrixTest {
+    @Test
+    public void testInsertAtBeginning() {
+        Integer[][] data = {{1, 2}, {3, 4}};
+        Matrix<Integer> matrix = new Matrix<>(2, 2, data);
+        matrix.insertAtBeginning(0);
+
+        Integer[][] expected = {{0, 0}, {1, 2}, {3, 4}};
+        assertArrayEquals(expected, matrix.getData());
+    }
+
+    @Test
+    public void testInsertAtBeginningEmptyMatrix() {
+        Integer[][] emptyData = {};
+        Matrix<Integer> matrix = new Matrix<>(0, 0, emptyData);
+        matrix.insertAtBeginning(5);
+
+        Integer[][] expected = {{5}};
+        assertArrayEquals(expected, matrix.getData());
+    }
 }
