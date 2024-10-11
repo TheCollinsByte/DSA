cf3dd0032641570fbbb0dc97f095930e98418d1e
colspan <collin@collindigm.com>
2024-10-11 13:34:10 +0300

    Queue: Array class methods implemented for enqueue

---
+++ ds-queue/src/main/java/com/thecollinsbyte/dsa/ArrayQueue.java
@@ -0,0 +1,69 @@
+package com.thecollinsbyte.dsa;
+
+public class ArrayQueue<T> implements Queue<T> {
+    private static final int DEFAULT_CAPACITY = 10;
+    private T[] queue;
+    private int front;
+    private int rear;
+    private int size;
+
+    @SuppressWarnings("unchecked")
+    public ArrayQueue() {
+        queue = (T[]) new Object[DEFAULT_CAPACITY];
+        front = 0;
+        rear = -1;
+        size = 0;
+    }
+
+    @Override
+    public void enqueue(T item) {
+        if(isFull()) {
+            resize();
+        }
+        rear = (rear + 1) % queue.length;
+        queue[rear] = item;
+        size++;
+    }
+
+    @Override
+    public T dequeue() {
+        return null;
+    }
+
+    @Override
+    public T peek() {
+        return null;
+    }
+
+    @Override
+    public T rear() {
+        return null;
+    }
+
+    @Override
+    public boolean isFull() {
+        return size == queue.length;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @SuppressWarnings("unchecked")
+    private void resize() {
+        int newCapacity = queue.length * 2;
+        T[] newQueue = (T[]) new Object[newCapacity];
+        for (int i = 0; i < size; i++) {
+            newQueue[i] = queue[(front + i) % queue.length];
+        }
+        queue = newQueue;
+        front = 0;
+        rear = size - 1;
+    }
+}
