2a4dc0455e7931d65edfa4cde4e9be208aecabd9
colspan <collin@collindigm.com>
2024-10-14 18:53:07 +0300

    Hashing: Test Env updated to test for collision within the test cases

--- ds-hashing/src/test/java/com/thecollinsbyte/dsa/HashTableTest.java
+++ ds-hashing/src/test/java/com/thecollinsbyte/dsa/HashTableTest.java
@@ -1,7 +1,6 @@
 package com.thecollinsbyte.dsa;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.Optional;
 
@@ -9,49 +8,101 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class HashTableTest {
-
-    private HashTable<String, Integer> hashTable;
+    private HashTable<TestKey, Integer> hashTable;
 
     @BeforeEach
-    public void setUp() {
+    void setUp() {
         hashTable = new HashTable<>();
     }
 
-
     @Test
-    public void testPutAndGet() {
-        hashTable.put("One", 1);
-        hashTable.put("Two", 2);
-        hashTable.put("Three", 3);
+    void testPutAndGet() {
+        TestKey key1 = new TestKey("one", 1);
+        TestKey key2 = new TestKey("two", 2);
+        TestKey key3 = new TestKey("three", 3);
 
-        assertEquals(Optional.of(1), hashTable.get("One"));
-        assertEquals(Optional.of(2), hashTable.get("Two"));
-        assertEquals(Optional.of(3), hashTable.get("Three"));
-        assertEquals(Optional.empty(), hashTable.get("Four"));
+        hashTable.put(key1, 1);
+        hashTable.put(key2, 2);
+        hashTable.put(key3, 3);
+
+        assertEquals(Optional.of(1), hashTable.get(key1));
+        assertEquals(Optional.of(2), hashTable.get(key2));
+        assertEquals(Optional.of(3), hashTable.get(key3));
+        assertEquals(Optional.empty(), hashTable.get(new TestKey("four", 4)));
     }
 
     @Test
-    public void testPutOverwrite() {
-        hashTable.put("Key", 1);
-        assertEquals(Optional.of(1), hashTable.get("Key"));
+    void testPutOverwrite() {
+        TestKey key = new TestKey("key", 1);
+        hashTable.put(key, 1);
+        assertEquals(Optional.of(1), hashTable.get(key));
 
-        hashTable.put("Key", 2);
-        assertEquals(Optional.of(2), hashTable.get("Key"));
+        hashTable.put(key, 2);
+        assertEquals(Optional.of(2), hashTable.get(key));
     }
 
     @Test
-    public void testRemove() {
-        hashTable.put("Key", 1);
-        assertEquals(Optional.of(1), hashTable.get("Key"));
-        hashTable.remove("Key");
-        assertEquals(Optional.empty(), hashTable.get("Key"));
+    void testRemove() {
+        TestKey key = new TestKey("key", 1);
+        hashTable.put(key, 1);
+        assertEquals(Optional.of(1), hashTable.get(key));
+
+        hashTable.remove(key);
+        assertEquals(Optional.empty(), hashTable.get(key));
     }
 
     @Test
-    public void testSize() {
+    void testSize() {
+        assertEquals(0, hashTable.size());
+
+        hashTable.put(new TestKey("one", 1), 1);
+        assertEquals(1, hashTable.size());
+
+        hashTable.put(new TestKey("two", 2), 2);
+        assertEquals(2, hashTable.size());
+
+        hashTable.remove(new TestKey("one", 1));
+        assertEquals(1, hashTable.size());
     }
 
     @Test
-    public void testCollisionHandling() {
+    void testCollisionHandling() {
+        TestKey key1 = new TestKey("First", 1);
+        TestKey key2 = new TestKey("Second", 1);  // Same hash as key1
+        TestKey key3 = new TestKey("Third", 2);   // Different hash
+
+        hashTable.put(key1, 1);
+        hashTable.put(key2, 2);
+        hashTable.put(key3, 3);
+
+        assertEquals(Optional.of(1), hashTable.get(key1));
+        assertEquals(Optional.of(2), hashTable.get(key2));
+        assertEquals(Optional.of(3), hashTable.get(key3));
+
+        assertEquals(3, hashTable.size());
+    }
+
+    // Custom key class for testing collisions
+    private static class TestKey {
+        private final String value;
+        private final int hash;
+
+        TestKey(String value, int hash) {
+            this.value = value;
+            this.hash = hash;
+        }
+
+        @Override
+        public int hashCode() {
+            return hash;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj) return true;
+            if (obj == null || getClass() != obj.getClass()) return false;
+            TestKey testKey = (TestKey) obj;
+            return value.equals(testKey.value);
+        }
     }
 }
