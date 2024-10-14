48c155453647d68626832d3c690e4d333a7ac429
colspan <collin@collindigm.com>
2024-10-14 17:20:00 +0300

    Hash Table: put method implemented

--- ds-hashing/src/main/java/com/thecollinsbyte/dsa/HashTable.java
+++ ds-hashing/src/main/java/com/thecollinsbyte/dsa/HashTable.java
@@ -1,7 +1,7 @@
 package com.thecollinsbyte.dsa;
 
+import java.lang.Math;
 import java.util.LinkedList;
-import java.util.Map.Entry;
 
 public class HashTable<K, V> {
     private static final int DEFAULT_CAPACITY = 16;
@@ -17,4 +17,42 @@ public class HashTable<K, V> {
         }
     }
 
+    public void put(K key, V value) {
+        int index = hash(key);
+        for (Entry<K, V> entry : buckets[index]) {
+            if (entry.key.equals(key)) {
+                entry.value = value;
+                return;
+            }
+        }
+        buckets[index].add(new Entry<>(key, value));
+        size++;
+    }
+
+    public V get(K key) {
+
+        return null;
+    }
+
+    public V remove(K key) {
+        return null;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    private int hash(K key) {
+        return Math.abs(key.hashCode() % buckets.length);
+    }
+
+    private static class Entry<K, V> {
+        K key;
+        V value;
+
+        Entry(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
 }
