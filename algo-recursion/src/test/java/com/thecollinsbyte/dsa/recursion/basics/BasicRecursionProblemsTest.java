8da6647699eb3be203e24770df64d57b9e78bfae
colspan <collin@collindigm.com>
2024-10-29 11:12:23 +0300

    Recursion Test Class initialized

---
+++ algo-recursion/src/test/java/com/thecollinsbyte/dsa/recursion/basics/BasicRecursionProblemsTest.java
@@ -0,0 +1,19 @@
+package com.thecollinsbyte.dsa.recursion.basics;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class BasicRecursionProblemsTest {
+
+    BasicRecursionProblems recursion;
+
+    @BeforeEach
+    public void setUp() {
+        recursion = new BasicRecursionProblems();
+    }
+
+    @Test
+    public void testDecimalToBinary() {
+    }
+
+}
